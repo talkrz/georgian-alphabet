e791364b0a2f52e1538694c91030d8e880e691ec
talkrz <krzysztof.talajko@gmail.com>
2017-02-15 23:12:55 +0100

    The app origin

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>ქართული დამწერლობა</h2>
+        </div>
+        <p className="App-intro">
+          WIP
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
