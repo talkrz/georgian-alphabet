90b62ff7f2c732e93719a8b4dfb9398b0fdcee64
talkrz <krzysztof.talajko@gmail.com>
2017-02-16 00:55:48 +0100

    Get started with redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import Alphabet from './components/Alphabet/Alphabet';
 import LearnLetters from './components/LearnLetters/LearnLetters';
 import Intro from './pages/Intro';
 import App from './pages/App';
+import reducers from './redux/reducers';
 import './index.css';
 
+const store = createStore(
+  combineReducers(reducers),
+  applyMiddleware(thunk),
+);
+
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Alphabet}/>
-      <Route path="/alphabet" component={Alphabet}/>
-      <Route path="/learn-letters" component={LearnLetters}/>
-    </Route>
-    <Route path="/intro" component={Intro}/>
-  </Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Alphabet}/>
+        <Route path="/alphabet" component={Alphabet}/>
+        <Route path="/learn-letters" component={LearnLetters}/>
+      </Route>
+      <Route path="/intro" component={Intro}/>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
